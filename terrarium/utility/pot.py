054acd6299ce47a41d0a7f903248e8a5fca7c231
Roy <crosscent@gmail.com>
2016-04-24 13:59:31 -0700

    Calculate optimal number of seed clusters in a circular pot
    From the optimal seed cluster radius, calculate the maximum number of clusters that can be planted in a circular pot

---
+++ terrarium/utility/pot.py
@@ -0,0 +1,97 @@
+import math
+
+class Cluster(object):
+    """Cluster class
+
+    The basic class of a cluster of seeds.
+
+    Attributes:
+        radius: The radius of a cluster of seeds
+    """
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_radius(self):
+        return self.radius
+
+class Pot(object):
+    """Pot class
+
+    The basic class of a pot for planting.
+
+    Attributes:
+        radius: The radius of the pot
+    """
+
+    def __init__(self, radius):
+        """Initialize the Pot class with radius
+        """
+        self.radius = radius
+        self.cluster = []
+
+    def get_radius(self):
+        """Returns the radius of the pot
+        """
+        return self.radius
+
+    def add_cluster(self, cluster):
+        """Add a cluster of seeds to the pot
+
+        Args:
+            cluster: A cluster instance
+        """
+        self.cluster.append(cluster)
+
+    def cluster_calculation(self, seed_cluster_radius, radius_difference):
+        """Returns the maximum number of clusters of the type of seed inputted
+        that can be planted in the Pot.
+        """
+
+        number_of_circles = math.floor((2 * math.pi * radius_difference) / (2 * seed_cluster_radius))
+        x0 = radius_difference * math.cos(0 * 2 * math.pi / number_of_circles)
+        x1 = radius_difference * math.cos(1 * 2 * math.pi / number_of_circles)
+        y0 = radius_difference * math.sin(0 * 2 * math.pi / number_of_circles)
+        y1 = radius_difference * math.sin(1 * 2 * math.pi / number_of_circles)
+        distance = math.pow((math.pow(x0 - x1, 2)) + (math.pow(y0 - y1, 2)), 0.5)
+        if distance < 2 * seed_cluster_radius:
+            number_of_circles -=1
+
+        new_radius_difference = radius_difference - (2 * seed_cluster_radius)
+        if new_radius_difference >= seed_cluster_radius:
+            return number_of_circles + self.cluster_calculation(seed_cluster_radius, new_radius_difference)
+        elif radius_difference > (2 * seed_cluster_radius):
+            return number_of_circles + 1
+        else:
+            return number_of_circles
+
+    def num_cluster_available(self, cluster):
+        """Returns the maximum number of clusters of the type of seed inputted
+        that can be planted in the given Pot.
+        """
+        if self.get_radius() < cluster.get_radius():
+            return "The minimum area for planing is {0}cm, and your pot only has an area of {1}".format(cluster.area, pot_area)
+
+        if self.get_radius() < cluster.get_radius() * 2:
+            return 1
+
+
+        radius_difference = self.get_radius() - cluster.get_radius()
+        return int(self.cluster_calculation(cluster.get_radius(), radius_difference))
+
+def circular_pot_calculation(pot_radius, cluster_radius):
+    """Calculate the number of seed clusters that can be planted in a given pot
+
+    Sometimes we want to see how many clusters of seeds we can plant in a 
+    large circular pot for a home garden, and this is useful for determining
+    that.
+
+    Args:
+        pot_radius: Radius of a pot
+        cluster_radius: Optimal radius of a cluster of seeds
+
+    Returns:
+        A integer of the maximum number of clusters that can fit into that pot
+    """
+    pot = Pot(radius=float(pot_radius))
+    seeds = Cluster(radius=float(cluster_radius))
+    return pot.num_cluster_available(seeds)
--- terrarium/web/api/calculation.py
+++ terrarium/web/api/calculation.py
@@ -2,9 +2,12 @@
 This module hosts all the basic calculations that can be done, basically
 anything that does not require database queries
 """
+
 from rest_framework import viewsets
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
+
+from terrarium.utility.pot import circular_pot_calculation
 
 class CalculationViewSet(viewsets.ViewSet):
     """
@@ -23,6 +26,21 @@ class CalculationViewSet(viewsets.ViewSet):
     def circular_pot(self, request):
         """
         GET method implementation of calculating pot size
-        """
-        return Response("pot size calculation")
 
+        Get args:
+            pot_radius: Radius of a pot
+            seed_radius: Optimal radius of a cluster of seeds
+
+        Returns:
+            A integer of the optimal number of clusters or an error message if
+            not all parameters are given
+        """
+        pot_radius = request.GET.get('pot_radius', None)
+        seed_radius = request.GET.get('seed_radius', None)
+
+        # check to see if all parameters are met
+        if not pot_radius or not seed_radius:
+            return Response("Please specify pot_radius, and seed_radius in your parameters")
+
+        # calculate number of clusters
+        return Response(circular_pot_calculation(pot_radius, seed_radius))
