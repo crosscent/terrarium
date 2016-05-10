dff4cfbc5c497d6423291f641634ac6b4f86d59d
Roy <crosscent@gmail.com>
2016-05-10 09:20:48 -0700

    OSM-Cache - Update Environment Variable for Postgis
    In order to use geodjango's capabilities, need Postgis connection.

--- provisioning/roles/common/templates/etc/profile.d/terrarium.sh
+++ provisioning/roles/common/templates/etc/profile.d/terrarium.sh
@@ -1,2 +1,2 @@
-export DATABASE_URL="postgresql://{{dbuser}}:{{dbpassword}}@localhost/{{dbname}}"
+export DATABASE_URL="postgis://{{dbuser}}:{{dbpassword}}@localhost/{{dbname}}"
 export TERRARIUM_SECRET_KEY={{TERRARIUM_SECRET_KEY}}
